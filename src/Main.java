8dd5d793ec16e4921dec0a1b69e9c557e05b1436
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-11 14:05:57 +0100

    Zwei Karten anlegen und eine ausspielen.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        Karte grün8 = new Karte(8, "grün");
+        Karte blau9 = new Karte(9, "blau");
+
+        grün8.ausspielen();
     }
 }
