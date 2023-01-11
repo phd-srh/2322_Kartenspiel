e0eb409c339d2e3a4eff07369e4068a9219f27a1
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-11 14:02:05 +0100

    Konstruktoren hinzugefügt.

--- src/Karte.java
+++ src/Karte.java
@@ -6,6 +6,25 @@ public class Karte {
     public String farbe;
     public String format = "standard";
 
+    public Karte() {
+        System.out.println("Neue Karte");
+    }
+
+    public Karte(int neuWert) {
+        wert = neuWert;
+    }
+
+    public Karte(int neuWert, String neuFarbe) {
+        wert = neuWert;
+        farbe = neuFarbe;
+    }
+
+    public Karte(int neuWert, String neuFarbe, String neuFormat) {
+        wert = neuWert;
+        farbe = neuFarbe;
+        format = neuFormat;
+    }
+
     // 2) Methoden / Operationen
     public void ziehen() {
         System.out.println("ziehen");
