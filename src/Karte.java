d5a3d28c9713c13ca6e85349a8dea45b4257094d
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-11 13:57:31 +0100

    Karte (Grundgerüst) implementiert.

--- src/Karte.java
+++ src/Karte.java
@@ -1,2 +1,17 @@
 public class Karte {
+    // Bestandteile einer Klasse
+    // 1) Attribute / Eigenschaften
+
+    public int wert;
+    public String farbe;
+    public String format = "standard";
+
+    // 2) Methoden / Operationen
+    public void ziehen() {
+        System.out.println("ziehen");
+    }
+
+    public void ausspielen() {
+        System.out.println("spiele: " + farbe + wert);
+    }
 }
